bebee34a0d9f50349970d28b9114085f77ab163f
wuscier <1352288123@qq.com>
2018-03-23 11:50:16 +0800

    add foreach_async_demo project

---
+++ foreach_async_demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace foreach_async_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            List<string> sList = new List<string>();
+            sList.Add("Michael Jordan");
+            sList.Add("Steve Jobs");
+            sList.Add("Bill Gates");
+            sList.Add("Justin Bieber");
+
+            sList.ForEach(async s =>
+            {
+
+                Console.WriteLine(s);
+
+                await p.RequestAsync(); 
+            });
+
+            Console.WriteLine("done.");
+            Console.ReadKey();
+        }
+
+        private async Task RequestAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string result = await httpClient.GetStringAsync("http://www.baidu.com");
+
+            Console.WriteLine(result);
+        }
+    }
+}
